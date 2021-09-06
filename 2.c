e9deceeeb25948a46c9d79fd7f35bd11d725902e
visal1 <90186463+visal1@users.noreply.github.com>
2021-09-06 15:43:55 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+void main()
+{
+ int a,b;
+ printf("enter the divisor:");
+ scanf("%d",&a);
+  printf("enter the divider:");
+  scanf("%d",&b);
+  if (a%b==0)
+    printf("divisible");
+  else
+    printf("non divisible");
+}
